5624a2cdfa9db6c7abfb00f376d7831866dcf5fa
Gergo Nagy <guzmop@gmail.com>
2017-10-18 17:52:18 +0100

    youtube api req test ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+
+import API_KEY from '../api_key'
+import SearchBar from './components/search_bar'
+
+YTSearch({key : API_KEY, term : 'surfboards'}, function(data){
+    console.log(data)
+})
+
 //Create a new component
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 //Generated a HTML and Pass to the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
